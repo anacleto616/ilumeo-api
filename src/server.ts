8aca1700eccb01dff421a372f50da0633a90d065
Rafael Anacleto <rafael.anacleto12@gmail.com>
2023-02-24 22:31:15 -0300

    chore: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
 
 app.listen(3333, () => {
   console.log('Server is running on http://localhost:3333');
