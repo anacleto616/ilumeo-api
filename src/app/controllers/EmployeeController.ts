cdfcd54e8d5668314d9287ab177c267cd4006989
Rafael Anacleto <rafael.anacleto12@gmail.com>
2023-02-26 20:17:59 -0300

    feat: implement functions index and access to EmployeeController

--- src/app/controllers/EmployeeController.ts
+++ src/app/controllers/EmployeeController.ts
@@ -1,8 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from 'express';
 import EmployeesRepository from '../repositories/EmployeesRepository';
 
 class EmployeeController {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async index(request: Request, response: Response): Promise<void | Record<string, any>> {
+    const employees = await EmployeesRepository.findAll();
+
+    response.json(employees);
+  }
+
   async show(request: Request, response: Response): Promise<void | Record<string, any>> {
     const { id } = request.params;
 
@@ -13,6 +19,22 @@ class EmployeeController {
     }
 
     response.json(employee);
+  }
+
+  async access(request: Request, response: Response): Promise<void | Record<string, any>> {
+    const { employee_code } = request.body;
+
+    if (!employee_code) {
+      return response.status(400).json({ error: 'Employee code is required.' });
+    }
+
+    const employee = await EmployeesRepository.findByEmployeeCode(employee_code);
+
+    if (!employee) {
+      return response.status(404).json({ error: 'Employee not found.' });
+    }
+
+    response.status(200).json({ message: 'Employee access successfully.'});
   }
 }
 
