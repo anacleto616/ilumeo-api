1388c0e727279f7d9ab3baa3963fdc4ebeba55b9
Rafael Anacleto <rafael.anacleto12@gmail.com>
2023-02-26 20:17:09 -0300

    feat: implement functions findAll and findByEmployeeCode to EmployeesRepository

--- src/app/repositories/EmployeesRepository.ts
+++ src/app/repositories/EmployeesRepository.ts
@@ -3,9 +3,23 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 class EmployeesRepository {
+  async findAll() {
+    const row = await prisma.employee.findMany();
+
+    return row;
+  }
+
   async findById(id: string) {
     const row = await prisma.employee.findUnique({
       where: { id }
+    });
+
+    return row;
+  }
+
+  async findByEmployeeCode(employeeCode: string) {
+    const row = await prisma.employee.findUnique({
+      where: { employee_code: employeeCode }
     });
 
     return row;
