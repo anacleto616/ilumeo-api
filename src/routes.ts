4a76b2c02a2249e4b0c5b1845d206d3e92be40d2
Rafael Anacleto <rafael.anacleto12@gmail.com>
2023-02-26 20:18:39 -0300

    feat: add routes /employee and /access

--- src/routes.ts
+++ src/routes.ts
@@ -7,6 +7,8 @@ const router = Router();
 router.get('/point-control-historic/:employeeId', PointControlHistoricController.index);
 router.post('/point-control-historic', PointControlHistoricController.store);
 
+router.get('/employee', EmployeeController.index);
 router.get('/employee/:id', EmployeeController.show);
+router.post('/access', EmployeeController.access);
 
 export default router;
