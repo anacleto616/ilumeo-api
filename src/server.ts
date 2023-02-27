0dd2d8cefebb273c9aa966d9234d70ca9c3f2476
Rafael Anacleto <rafael.anacleto12@gmail.com>
2023-02-27 11:28:39 -0300

    feat: add error handler

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import router from './routes';
 
@@ -8,6 +8,18 @@ app.use(express.json());
 app.use(cors());
 app.use(router);
 
+app.use(
+  (
+    error: ErrorRequestHandler,
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) => {
+    console.log(error);
+    response.sendStatus(500);
+  }
+);
+
 app.listen(3333, () => {
-  console.log('Server is running on http://localhost:3333');
+  console.log('🚀️ Server is running on http://localhost:3333');
 });
