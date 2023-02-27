35ccf2fb1912208d1f981ba8e50944c492af1028
Rafael Anacleto <rafael.anacleto12@gmail.com>
2023-02-27 11:04:44 -0300

    feat: add employee type

--- src/app/repositories/EmployeesRepository.ts
+++ src/app/repositories/EmployeesRepository.ts
@@ -1,15 +1,16 @@
 import { PrismaClient } from '@prisma/client';
+import { EmployeeType } from '../types/EmployeeType';
 
 const prisma = new PrismaClient();
 
 class EmployeesRepository {
-  async findAll() {
+  async findAll(): Promise<EmployeeType[]> {
     const row = await prisma.employee.findMany();
 
     return row;
   }
 
-  async findById(id: string) {
+  async findById(id: string): Promise<EmployeeType | null> {
     const row = await prisma.employee.findUnique({
       where: { id }
     });
@@ -17,7 +18,7 @@ class EmployeesRepository {
     return row;
   }
 
-  async findByEmployeeCode(employeeCode: string) {
+  async findByEmployeeCode(employeeCode: string): Promise<EmployeeType | null> {
     const row = await prisma.employee.findUnique({
       where: { employee_code: employeeCode }
     });
