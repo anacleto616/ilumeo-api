ff4e374232bdd196f09b75020b37f451dff115d8
Rafael Anacleto <rafael.anacleto12@gmail.com>
2023-02-27 13:52:06 -0300

    test: update test to util funtion toHourAndMinutes

--- src/app/utils/toHoursAndMinutes.spec.ts
+++ src/app/utils/toHoursAndMinutes.spec.ts
@@ -1,9 +1,11 @@
 import toHoursAndMinutes from "./toHoursAndMinutes";
 
 describe('To hours and minutes', () => {
-  it('Deve ser possivel converter minutos em horas e retornar uma string formatada', () => {
+  it('should be possible to convert minutes to hours and return a formatted string', () => {
     const hourFormatted = toHoursAndMinutes(480);
 
     expect(hourFormatted).toBe('8h');
+    expect(hourFormatted).not.toBe(8);
+    expect(hourFormatted).not.toBe('');
   });
 });
